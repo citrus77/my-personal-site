273fa122fa4b5cc42356ef3e493558a597228bc9
citrus77 <jason.lammers@outlook.com>
2021-12-16 18:24:03 -0800

    change background color

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,26 +3,23 @@ import dataExports from './data';
 
 const Contact = () => {
     const { getContactInfo } = dataExports;
-    const contactInfo = getContactInfo();
+    const { email, name, linkedIn, github} = getContactInfo();
     return (
         <div className='contact'>
             <h1>Get in touch!</h1>
             <div className='contact-info'>
-                <div>{contactInfo.name}</div>
-                <div>e-mail: 
-                    <a href='mailto:{contactInfo.email}'>
-                        {contactInfo.email}
+                <div>{name}</div>
+                <div>e-mail: <a href='mailto:{email}' target='_email'>
+                        {email}
                     </a>
                 </div>
-                <div>LinkedIn: 
-                    <a href={contactInfo.linkedIn} target='_linkedin'>
-                        {contactInfo.linkedIn}
+                <div>LinkedIn: <a href={linkedIn} target='_linkedin'>
+                        {linkedIn}
                     </a>
                 </div>
                 <br />
-                <div>Check out my GitHub @ 
-                    <a href={contactInfo.github} target='_github'>
-                        {contactInfo.github}
+                <div>Check out my GitHub @ <a href={github} target='_github'>
+                        {github}
                     </a>!
                 </div>
             </div>
