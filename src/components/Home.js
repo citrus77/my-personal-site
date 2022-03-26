12a3798b3353558e08ec78d9a8b04b70ca0c0021
citrus77 <jason.lammers@outlook.com>
2022-03-26 00:41:12 -0700

    added profile picture and revised Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     return <>
-        <div><i>This page is still under construction. This message will disappear when I feel it is complete.</i></div>
+        <div id='landing'>
+            <img src='img/profile.jpg' className='profile-pic' height='300' width='300' />
+            <span>Hello! I am a Software Engineer specializing in Full Stack Web Development. Check out my porfolio for some examples of my projects!</span>
+        </div>
     </>;
 };
 
